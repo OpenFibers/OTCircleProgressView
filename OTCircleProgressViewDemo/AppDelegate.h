ff05be522473d59aa32394156821422a49f8fb5c
openthread <openfibers@gmail.com>
2013-06-03 16:07:53 +0800

    Initial Commit

---
+++ OTCircleProgressViewDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  OTCircleProgressViewDemo
+//
+//  Created by openthread on 6/3/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ OTCircleProgressViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OTCircleProgressViewDemo
+//
+//  Created by openthread on 6/3/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
