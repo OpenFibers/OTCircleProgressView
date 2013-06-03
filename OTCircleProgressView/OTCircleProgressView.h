282d9678cc3823140a0524e683c6cdd040d05e6b
openthread <openfibers@gmail.com>
2013-06-03 16:50:31 +0800

    added OTCircleProgressView

---
+++ OTCircleProgressView/OTCircleProgressView.h
@@ -0,0 +1,28 @@
+//
+//  OTCircleProgressView.h
+//  OTCircleProgressViewDemo
+//
+//  Created by openthread on 6/3/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface OTCircleProgressView : UIView
+
+@property (nonatomic, retain) UIColor *trackTintColor;
+@property (nonatomic, retain) UIColor *progressTintColor;
+@property (nonatomic, assign) BOOL roundedCorners;
+@property (nonatomic, assign) CGFloat thicknessRatio;
+
+@property (nonatomic, assign) BOOL indeterminate;
+@property (nonatomic, assign) CGFloat indeterminateDuration;
+
+@property (nonatomic, assign) CGFloat beginPoint;
+@property (nonatomic, assign) CGFloat endPoint;
+
+@property (nonatomic, assign) CGFloat progress;
+- (void)setProgress:(CGFloat)progress animated:(BOOL)animated;
+
+@end
--- OTCircleProgressViewDemo/ViewController.m
+++ OTCircleProgressViewDemo/ViewController.m
@@ -7,23 +7,41 @@
 //
 
 #import "ViewController.h"
+#import "OTCircleProgressView.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+{
+    OTCircleProgressView *_circleProgressView;
+    NSTimer *_timer;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _circleProgressView = [[OTCircleProgressView alloc] initWithFrame:CGRectMake(120, 100, 80, 80)];
+    _circleProgressView.roundedCorners = YES;
+    [self.view addSubview:_circleProgressView];
+    
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.5f
+                                              target:self
+                                            selector:@selector(changeProgress)
+                                            userInfo:nil
+                                             repeats:YES];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)changeProgress
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    CGFloat progress = _circleProgressView.progress;
+    progress += 0.02;
+    if (progress > 1)
+    {
+        progress = 0;
+    }
+    [_circleProgressView setProgress:progress animated:YES];
 }
 
 @end
